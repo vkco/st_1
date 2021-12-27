6b2d881a925b527f5d4e85adee2bce26af48a2ad
stasmakii@gmail.com <stasmakii@gmail.com>
2021-12-27 21:36:28 +0300

    hm_1

---
+++ hm_1.rb
@@ -0,0 +1,10 @@
+puts "your name is bitch"
+name = gets.chomp
+puts "your height is a motherfucker"
+height = gets.chomp
+
+if ((height.to_i-110.0)*1.15) > 0
+puts " #{name} satisfactory son of a bitch "
+else
+puts " #{name} you are a fat motherfucker "
+end
